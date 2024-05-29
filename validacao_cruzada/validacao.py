5ffbb8916c0c0f83804111e9e4a1f72b5f9fdd72
REALFSB <fer.batista23@gmail.com>
2024-05-29 16:39:00 -0300

    Implementação da avaliação cruzada

--- validacao_cruzada/validacao.py
+++ validacao_cruzada/validacao.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score, KFold
 import numpy as np
 import pickle
 
@@ -84,6 +85,68 @@ melhores_parametros_redes = grid_search_arvore.best_params_
 melhores_resultado_redes = grid_search_arvore.best_score_
 
 ########################################################################################################################
+
+resultados_arvore = []
+resultados_randomforest = []
+resultados_knn = []
+resultados_logistica = []
+resultados_svn = []
+resultados_redeneural = []
+
+for i in range(30):
+    kfold = KFold(n_splits=10, shuffle=True, random_state=i)
+
+    arvore = DecisionTreeClassifier(criterion='entropy',
+                                    min_samples_leaf=1,
+                                    min_samples_split=5,
+                                    splitter='best')
+    scores = cross_val_score(arvore, x_credit, y_credit, cv=kfold)
+
+    resultados_arvore.append(scores.mean())
+
+    #####################################################################
+
+    random_forest = RandomForestClassifier(criterion='entropy',
+                                           min_samples_leaf=1,
+                                           min_samples_split=5,
+                                           n_estimators=10)
+
+    scores = cross_val_score(random_forest, x_credit, y_credit, cv=kfold)
+
+    resultados_randomforest.append(scores.mean())
+
+    #####################################################################
+
+    knn = KNeighborsClassifier()
+    scores = cross_val_score(knn, x_credit, y_credit, cv=kfold)
+
+    resultados_knn.append(scores.mean())
+
+    #####################################################################
+
+    logistica = LogisticRegression(C = 1.0,
+                                   solver='lbfgs',
+                                   tol=0.0001)
+    scores = cross_val_score(logistica, x_credit, y_credit, cv=kfold)
+
+    resultados_logistica.append(scores.mean())
+
+    #####################################################################
+
+    svn = SVC(kernel='rbf',
+              C = 2.0)
+    scores = cross_val_score(svn, x_credit, y_credit, cv=kfold)
+
+    resultados_svn.append((scores.mean()))
+
+    #####################################################################
+
+    rede_neural = MLPClassifier(activation='relu',
+                                batch_size=56,
+                                solver='adam')
+    scores = cross_val_score(rede_neural, x_credit, y_credit, cv=kfold)
+
+    resultados_redeneural.append(scores.mean())
 
 print(melhores_parametros_arvore)
 print(melhores_resultado_arvore)
