6226662b33ff8a05aa79c7ff188c51f5cc9d5f53
REALFSB <fer.batista23@gmail.com>
2024-06-16 20:56:47 -0300

    Pequenos ajustes

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import pickle
 
 ########################################################################################################################
